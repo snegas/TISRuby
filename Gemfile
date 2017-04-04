f6e9d9352ec9834ed15ddb57fd06a4d3e0833b2a
Alexander Snegovoy <alexander.snegovoy@dataart.com>
2017-04-04 13:23:33 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem "haml-rails", "~> 0.4.0"
+gem 'rmagick', '~> 2.15', '>= 2.15.4'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.5'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TISRuby::Application.config.secret_token = '2561277c544650b83ec8004b54429e445b9be57467fdb82cca80d8a9ed61800c813cb10f51882ac0daa10d7e00b13153f4b382715e663506f0ae028b2c1da95d'
